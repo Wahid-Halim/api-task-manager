151b7139a74c2a1ba4aae23d8e3650ebd3e81697
Wahid Halim <wahidkoe@gmail.com>
2025-11-03 12:38:18 +0430

    Implement Task.findOneAndDelete() on deleteTask controllers function

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -23,9 +23,7 @@ const getTask = async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
     if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `Could not find document with this ${taskID}` });
+      return res.status(404).json({ msg: `No Task with this ID: ${taskID}` });
     }
 
     res.status(200).json({ task });
@@ -39,8 +37,19 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send(" updated task");
 };
-const deleteTask = (req, res) => {
-  res.send(" deleted task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Not task with this ID:${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({
+      msg: error,
+    });
+  }
 };
 
 module.exports = {
