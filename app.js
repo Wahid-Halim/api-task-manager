8231b530b4b123b9c0760bbdfc292177b3a0a871
Wahid Halim <wahidkoe@gmail.com>
2025-11-03 12:09:45 +0430

    Add controllers folder with tasks.js file Add routes folder Add models folder Add db and connect db

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const taskRouter = require("./routes/task");
+
+// .env package
+require("dotenv").config();
+// db connect
+const connectDB = require("./db/connect");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.use("/api/v1/tasks", taskRouter);
+
+const PORT = 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(5000, () => {
+      console.log("server is running on port " + PORT);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
---
+++ controllers/tasks.js
@@ -0,0 +1,52 @@
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(201).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `Could not find document with this ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({
+      msg: error,
+    });
+  }
+};
+
+const updateTask = (req, res) => {
+  res.send(" updated task");
+};
+const deleteTask = (req, res) => {
+  res.send(" deleted task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
